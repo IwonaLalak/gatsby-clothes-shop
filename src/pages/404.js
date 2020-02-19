64ed038e8496371acc43802084bfce8b4b0c8d2a
Iwona Lalak <ilalak@topsa.com.pl>
2020-02-19 12:55:37 +0100

    basic structure of layout, stylesheet files

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from "react"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/shared/layout/layout"
+import SEO from "../components/shared/layout/seo"
 
 const NotFoundPage = () => (
   <Layout>
