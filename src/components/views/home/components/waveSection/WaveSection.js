2c919a5344158de08a94696b9eff6f3126c1f2f0
Iwona Lalak <ilalak@topsa.com.pl>
2020-02-21 11:14:58 +0100

    wave component, fix svg path

--- src/components/views/home/components/waveSection/WaveSection.js
+++ src/components/views/home/components/waveSection/WaveSection.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import {SVG} from "../../../../shared/svg/SVG";
 import {pathBottom, pathDimensions, pathTOP} from "../../../../../utilities/svgPaths";
+import {Col, Container, Row} from "react-bootstrap";
 
 const WaveSection = () => {
     return (
         <section id={'WAVE'}>
             <SVG fill={'#DDD'} viewbox={pathDimensions()} path={pathTOP()}/>
-            <h1>
-                wave
-            </h1>
+            <Container>
+                <Row>
+                    <Col lg={6} xl={6}>
+                        <div className={'wave-column first'}>
+                            <h1>FINAL SALE</h1>
+                            <h2>
+                                up to -70% <br />
+                                until 04/2020
+                            </h2>
+                        </div>
+                    </Col>
+                    <Col lg={6} xl={6}>
+                        <div className={'wave-column last'}>
+                            <h1>NEXT COLLECTION</h1>
+                            <h2>
+                                spring 2020 <br />
+                                from 03/2020
+                            </h2>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
             <SVG fill={'#DDD'} viewbox={pathDimensions()} path={pathBottom()}/>
 
         </section>
