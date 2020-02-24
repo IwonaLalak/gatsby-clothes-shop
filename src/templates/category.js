03c3842163552c2311b4a088de7a238bd72c35c5
Iwona Lalak <ilalak@topsa.com.pl>
2020-02-24 16:32:50 +0100

    print data

--- src/templates/category.js
+++ src/templates/category.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import Layout from "../components/shared/layout/layout";
 import SEO from "../components/shared/seo/seo";
+import {graphql, Link} from 'gatsby';
 
-const Category = ({pageContext}) => {
+const Category = (props) => {
+    let {pageContext} = props;
+    let {data: {allProduct}} = props;
+
+    const checkIfThereAreSomeProducts = (subcategory_id) => {
+
+        let products = allProduct.edges.filter(item => subcategory_id === item.node.subcategory_id);
+        if (products.length > 0)
+            return products.length
+        else
+            return 0
+    };
+
     return (
         <Layout>
-            <SEO title={'category page'} />
+            <SEO title={'category page'}/>
             <h1>
                 Category: {pageContext.category_name}
+
             </h1>
+            <h3>
+                {allProduct.edges.length} products
+            </h3>
+            <h5>subcategories:</h5>
+            <ul>
+                {
+                    pageContext.subcategories.map(subcategory => <li>
+                        <Link
+                            to={pageContext.category_url + subcategory.subcategory_url}>
+                            {subcategory.subcategory_name}
+                        </Link>
+                        <span style={{marginLeft: '15px'}}>
+                            {checkIfThereAreSomeProducts(subcategory.subcategory_id)} products
+                        </span>
+                    </li>)
+                }
+            </ul>
             <div>
                 {
                     JSON.stringify(pageContext, undefined, 2)
@@ -17,5 +48,20 @@ const Category = ({pageContext}) => {
         </Layout>
     );
 };
+
+export const query = graphql`
+    query getProductsInCategory($category_id: Int!) {
+      allProduct(filter: {category_id: {eq: $category_id}}) {
+        edges {
+          node {
+            id
+            product_name
+            category_id
+            subcategory_id
+          }
+        }
+      }
+}
+`;
 
 export default Category;
--- src/templates/subcategory.js
+++ src/templates/subcategory.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import Layout from "../components/shared/layout/layout";
 import SEO from "../components/shared/seo/seo";
+import {graphql, Link} from "gatsby";
 
-const Subcategory = ({pageContext}) => {
+const Subcategory = (props) => {
+
+    let {pageContext} = props;
+    let {data: {allProduct}} = props;
+
     return (
         <Layout>
-            <SEO title={'subcategory page'} />
+            <SEO title={'subcategory page'}/>
             <h1>subcategory: {pageContext.subcategory_name}</h1>
+            <h5>products in subcategory:</h5>
+            <ul>
+                {
+                    allProduct.edges.map(({node}) =>
+
+                        <li>
+                            <Link to={
+                                node.productCategory.category_url
+                                +
+                                node.productSubcategory.subcategory_url
+                                +
+                                node.product_url
+                            }>
+                                {node.product_name}
+                            </Link>
+                        </li>
+                    )
+                }
+            </ul>
             <div>
                 {
                     JSON.stringify(pageContext, undefined, 2)
@@ -15,5 +39,30 @@ const Subcategory = ({pageContext}) => {
         </Layout>
     );
 };
+
+export const query = graphql`
+    query getProductsInSubcategory($subcategory_id: Int!) {
+      allProduct(filter: {subcategory_id: {eq: $subcategory_id}}) {
+        edges {
+          node {
+            id
+            product_name
+            product_url
+            subcategory_id
+            productCategory {
+              category_id
+              category_name
+              category_url
+            }
+            productSubcategory {
+              subcategory_id
+              subcategory_name
+              subcategory_url
+            }
+          }
+        }
+      }
+}
+`;
 
 export default Subcategory;
