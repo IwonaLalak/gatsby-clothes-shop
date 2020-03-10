61127a0ddb9c293867ad422bbb88a0c7b1455835
Iwona Lalak <ilalak@topsa.com.pl>
2020-03-10 15:27:46 +0100

    category page

--- src/components/views/category/components/sections/CategoryHeaderComponent.js
+++ src/components/views/category/components/sections/CategoryHeaderComponent.js
@@ -16,8 +16,7 @@ const CategoryHeaderSection = ({category}) => {
                             industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
                             of type and
                             scrambled it to make a type specimen book. It has survived not only five centuries, but also
-                            the leap
-                            into electronic typesetting, remaining essentially unchanged.
+                            the leap into electronic typesetting, remaining essentially unchanged.
                         </p>
                     </Col>
                 </Row>
