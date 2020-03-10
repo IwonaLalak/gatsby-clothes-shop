d2b154ea5c57f7cac45ac8d58c40db0d0c893839
Iwona Lalak <ilalak@topsa.com.pl>
2020-03-10 11:15:22 +0100

    search form styles

--- src/components/shared/layout/HeaderComponents/Search.js
+++ src/components/shared/layout/HeaderComponents/Search.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {IconSearch} from "../../icons/FontAwesomeIcons";
 import {navigate} from 'gatsby';
+import {Button, FormControl} from "react-bootstrap";
 
 const Search = () => {
 
@@ -9,15 +10,15 @@ const Search = () => {
     const onSubmitSearch = (e) => {
         e.preventDefault();
         if (searchInput.current.value.length > 0 && searchInput.current.value.length < 100) {
-            navigate("/search", {state: {searchedValue:searchInput.current.value}});
+            navigate("/search", {state: {searchedValue: searchInput.current.value}});
         }
     };
 
     return (
         <div id={'search-nav'}>
             <form onSubmit={onSubmitSearch}>
-                <input type={'search'} ref={searchInput} placeholder={'product or category'}/>
-                <button type={'submit'}><IconSearch/></button>
+                <FormControl type={'search'} ref={searchInput} placeholder={'Category or product'}/>
+                <Button variant={'default'} type={'submit'}><IconSearch/></Button>
             </form>
         </div>
     );
