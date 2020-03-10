c6e972f55119f329ab1c4fe94a539cc628279a66
Iwona Lalak <ilalak@topsa.com.pl>
2020-03-10 09:41:02 +0100

    some fix in stylesheets

--- src/components/shared/svg/SVG.js
+++ src/components/shared/svg/SVG.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const SVG = ({viewbox,path,fill}) => (
-    <svg viewBox={viewbox} preserveAspectRatio={"none"}>
+    <svg viewBox={viewbox} preserveAspectRatio={"none"} className={'svg-wave'}>
         <path d={path}
               style={{fill}}>
         </path>
