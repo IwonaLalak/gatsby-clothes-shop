91b0d09821f20e371afa941e9b83ace2724d860d
Iwona Lalak <ilalak@topsa.com.pl>
2020-03-12 13:44:13 +0100

    commit current changes

--- src/components/views/subcategory/components/sections/SubcategoryProducts.js
+++ src/components/views/subcategory/components/sections/SubcategoryProducts.js
@@ -5,15 +5,15 @@ import ProductComponent from "../../../../product/ProductComponent";
 const SubcategoryProducts = ({products}) => {
     return (
         <section id={'SUBCATEGORYPRODUCTS'}>
-                <Row>
-                    {
-                        products.map(item=>
-                            <Col xs={12} sm={12} md={6} lg={6} xl={4}>
-                                <ProductComponent product={item.node}/>
-                            </Col>
-                        )
-                    }
-                </Row>
+            <Row>
+                {
+                    products.map(item =>
+                        <Col xs={12} sm={12} md={6} lg={6} xl={4}>
+                            <ProductComponent product={item.node}/>
+                        </Col>
+                    )
+                }
+            </Row>
         </section>
     );
 };
